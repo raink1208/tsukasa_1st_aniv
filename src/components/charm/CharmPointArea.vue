85c10f920c4cc791c599caa4c748a122047ff9fa
raink1208 <58714901+raink1208@users.noreply.github.com>
2024-12-19 22:42:19 +0900

    魅力追加

--- src/components/charm/CharmPointArea.vue
+++ src/components/charm/CharmPointArea.vue
@@ -1,4 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ImageSwiperSlide from '@/components/ImageSwiperSlide.vue'
+
+const horrorImages = [
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/first_stream_face.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/first_stream_charm.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/first_stream_guruguru.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/school_outfit_face.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/school_outfit_smile.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/3D_hypnosis.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/3D_chair.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/profile_slide/3D_weapon.webp",
+];
+
+const vlogImages = [
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/1_thumbnail.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/1_hitumabusi.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/1_massage.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/1_akiba.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/2_thumbnail.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/2_bonnnou.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/2_tororo.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/2_zazenn.webp",
+  "https://1staniv-cdn.tsukasa-kingdom.net/vlog_slide/2_ecchi.webp",
+]
+</script>
 
 <template>
   <div id="charm">
@@ -7,51 +32,49 @@
         <img data-anim-slide="bottomIn" alt="魅力 Charm" src="" />
       </h2>
       <div class="point">
-        <h3 class="title">ホラーゲーム</h3>
+        <h3 class="title fade-in" data-anim-slide="bottomIn">ホラーゲーム</h3>
         <div class="intro">
-          <div class="intro-body">
-            <p></p>
-            <div class="image-wrapper">
-              <img src="/src/assets/img/history/first_streaming.webp" />
-            </div>
-          </div>
-          <div class="video-wrapper">
-            <video />
-          </div>
-        </div>
-      </div>
-      <div class="point">
-        <h3 class="title">VLog</h3>
-        <div class="intro">
-          <div class="intro-video">
-            <div class="video-wrapper">
-              <video />
-            </div>
-          </div>
-          <div class="intro-body">
-            <p>
-              紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文
+          <div class="intro-text">
+            <p class="fade-in" data-anim-slide="leftIn">
+              <b>「ホラーゲームといえば領国つかさ」</b><br>
+              デビューから1年で 50タイトル以上 200時間越えと数多くのホラゲー配信をしてきた彼女。<br>
+              暗闇とジャンプスケアが苦手であり、強がりながらも可愛らしいリアクションやガチの悲鳴で領国民達を魅了しています。
+              恐怖感を楽しむだけではなく、考察しながら進めたり推理することでストーリーの真相に迫っていく姿は真のホラゲー好きと言えます。<br>
+              ホラーが苦手な領国民でも閣下のかわいいリアクションを楽しむことで恐怖心を和らげ、ストーリーを理解しつつ配信を楽しむことができるでしょう。
             </p>
+          </div>
+          <div class="intro-image">
             <div class="image-wrapper">
-              <img src="/src/assets/img/history/first_streaming.webp" />
+              <ImageSwiperSlide :images="horrorImages" />
             </div>
           </div>
         </div>
       </div>
       <div class="point">
-        <h3 class="title">雑談</h3>
+        <h3 class="title fade-in" data-anim-slide="bottomIn">旅行の感想 + VLog</h3>
         <div class="intro">
-          <div class="intro-body">
-            <p>
-              紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文
-            </p>
+          <div class="intro-image">
             <div class="image-wrapper">
-              <img src="/src/assets/img/history/first_streaming.webp" />
+              <ImageSwiperSlide :images="vlogImages" />
             </div>
           </div>
-          <div class="intro-video">
-            <div class="video-wrapper">
-              <video />
+          <div class="intro-text">
+            <p class="fade-in" data-anim-slide="rightIn">
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="point">
+        <h3 class="title fade-in" data-anim-slide="bottomIn">無軌道雑談</h3>
+        <div class="intro">
+          <div class="intro-text">
+            <p class="fade-in" data-anim-slide="leftIn">
+              紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文紹介文
+            </p>
+          </div>
+          <div class="intro-image">
+            <div class="image-wrapper">
+              <ImageSwiperSlide :images="horrorImages" />
             </div>
           </div>
         </div>
@@ -73,10 +96,6 @@ h3 {
   max-width: 1580px;
 }
 
-.point {
-  height: 800px;
-}
-
 .title {
   padding-left: 20px;
 }
@@ -86,35 +105,38 @@ h3 {
   gap: 5%;
 }
 
-.intro-body {
-  width: 40%;
+.intro-text {
+  width: 45%;
   padding-top: 30px;
 }
 
-.intro-body p {
-  font-size: 18px;
+.intro-text p {
+  font-size: 19px;
 }
 
-.intro-video {
-  width: 55%;
+.intro-image {
+  width: 50%;
 }
 
-.intro-video {
+@media screen and (max-width: 1050px) {
+  .intro {
+    flex-direction: column;
+  }
+
+  .intro-text {
+    order: -1;
+    width: revert;
+  }
+
+  .intro-image {
+    width: revert;
+  }
 }
 
-.image-wrapper {
-  border-radius: 16px;
-  overflow: hidden;
+@media screen and (max-width: 768px) {
+  .inner-wrapper {
+    padding: 0 2em;
+  }
 }
 
-.video-wrapper {
-  margin-top: 120px;
-  overflow: hidden;
-  border-radius: 10px;
-}
-
-.video-wrapper video {
-  width: 100%;
-  height: 100%;
-}
 </style>
